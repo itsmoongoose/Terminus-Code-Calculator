658dc4e18481f3845da95214d071e7a01c2163ea
itsmoongoose <lewishlemay@gmail.com>
2025-07-05 14:17:49 -0600

    Ask user for input of x y z

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,27 @@
 //Function Main
 async function main() {
 
+    //User Input -- x
+    let x = await input("What is X: ");
+    x = x.trim();
+    x = Number(x);
+
+    //User Input -- y
+    let y = await input("What is Y: ");
+    y = y.trim();
+    y = Number(y);
+
+    //User Input -- z
+    let z = await input("What is Z: ");
+    z = z.trim();
+    z = Number(z);
+
+
     //code1
     const equation1 = ((2*x) + 11);
 
-
-
-
     //code2
     const equation2 = (((2*z) + y) - 5);
-
-
-
 
     //code3
     const equation3 = ((y + z) - x);
